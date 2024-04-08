7232ce425f7d8b75e35d5c81663bfa894df9c3dc
Pushpendra-1697 <104748364+Pushpendra-1697@users.noreply.github.com>
2024-04-08 09:14:58 +0530

    added sorting and filtering

--- server/Routes/Dashboard.route.js
+++ server/Routes/Dashboard.route.js
@@ -2,13 +2,27 @@ const { Router } = require('express');
 const { DashboardModel } = require('../Models/dashboard.model');
 const dashboardRouter = Router();
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
 
 
 dashboardRouter.get('/', async (req, res) => {
     const { token } = req.headers;
     userId = jwt.decode(token, process.env.secret_key).id;
+    const { sort, order } = req.query;
+
     try {
-        const bugs = await DashboardModel.find({ userId });
+        let bugs;
+        if (sort !== 'undefined' && order !== 'undefined') {
+            let sortOptions = {};
+            if (order === 'asc') {
+                sortOptions.priority = 1;
+            } else {
+                sortOptions.priority = -1;
+            }
+            bugs = await DashboardModel.find({ userId }).sort(sortOptions);
+        } else {
+            bugs = await DashboardModel.find({ userId });
+        }
         res.send(bugs);
     } catch (err) {
         res.status(404).send({ Error: err.message });
